596c505a0b67851246006faf000263a7046626fe
Orekidesu <cmcodog10@gmail.com>
2024-02-22 13:42:58 +0800

    installs

--- src/components/login_component.vue
+++ src/components/login_component.vue
@@ -14,9 +14,9 @@ const signInWithEmail = async (email: string, password: string) => {
   try {
     await signInWithEmailAndPassword(auth, email, password);
     if (rememberMe.value) {
-        localStorage.setItem('rememberMe', email);
+      localStorage.setItem('rememberMe', email);
     } else {
-        localStorage.removeItem('rememberMe');
+      localStorage.removeItem('rememberMe');
     }
     window.alert('You have successfully signed in');
     router.push('/dashboard');
@@ -60,7 +60,7 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="flex items-center ms-3">
+  <div class="flex items-center ms-3">
     <Button variant="ghost" size="sm" class="mr-0" @click="toggleDarkMode">
       <span class="material-symbols-outlined" v-if="darkMode">
         light_mode
@@ -70,40 +70,41 @@ onMounted(() => {
       </span>
     </Button>
   </div>
-    <div
-        class="w-80 sm:w-96 p-8 bg-white border border-gray-200 rounded-lg shadow md:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <form class="space-y-6"  @submit.prevent="signInWithEmail(email,password)">
-            <h5 class="text-xl font-medium text-gray-900 dark:text-white">Login to Clairenet Billing System</h5>
-            <div>
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                <input v-model="email" type="email" name="email" id="email"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                    placeholder="name@company.com" required />
-            </div>
-            <div>
-                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    Password</label>
-                <input v-model="password" type="password" name="password" id="password" placeholder="••••••••"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                    required />
-            </div>
-            <div class="flex items-start">
-                <div class="flex items-start">
-                    <div class="flex items-center h-5">
-                        <input v-model="rememberMe" id="remember" type="checkbox" value=""
-                            class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                             />
-                    </div>
-                    <label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember
-                        me</label>
-                </div>
-                <!-- <a href="#" class="ms-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a> -->
-            </div>
-            <button type="submit"
-                class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login
-            to your account</button>
-        <!-- <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+  <div
+    class="w-80 sm:w-96 p-8 bg-white border border-gray-200 rounded-lg shadow md:p-8 dark:bg-gray-800 dark:border-gray-700">
+    <form class="space-y-6" @submit.prevent="signInWithEmail(email, password)">
+      <h5 class="text-xl font-medium text-gray-900 dark:text-white">Login to Clairenet Billing System</h5>
+      <div>
+        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+        <input v-model="email" type="email" name="email" id="email"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+          placeholder="name@company.com" required />
+      </div>
+      <div>
+        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Password</label>
+        <input v-model="password" type="password" name="password" id="password" placeholder="••••••••"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+          required />
+      </div>
+      <div class="flex items-start">
+        <div class="flex items-start">
+          <div class="flex items-center h-5">
+            <input v-model="rememberMe" id="remember" type="checkbox" value=""
+              class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" />
+          </div>
+          <label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember
+            me</label>
+        </div>
+        <!-- <a href="#" class="ms-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a> -->
+      </div>
+      <button type="submit"
+        class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login
+        to your account</button>
+      <!-- <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
             Not registered? <a href="#" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
         </div> -->
     </form>
-</div></template>
+
+  </div>
+</template>
