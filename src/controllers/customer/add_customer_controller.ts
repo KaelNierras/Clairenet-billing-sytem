64ecb1434cff2e2781199c1157cf1b97160aa7c6
Kyle Anthony Nierras <20-1-01205@vsu.edu.ph>
2024-02-25 02:11:59 +0800

    Update add_customer_controller.ts

--- src/controllers/customer/add_customer_controller.ts
+++ src/controllers/customer/add_customer_controller.ts
@@ -4,36 +4,31 @@ import { ref } from "vue";
 import { fetchCustomers } from "./table_dashboard_controller";
 
 export function useAddCustomerController() {
+    // Reactive Variables
+    const customerName = ref('');
+    const selectedMunicipality = ref('');
+    const selectedBarangay = ref('');
 
-//Reactive Variable
-const customerName = ref('')
-const selectedMunicipality = ref('');
-const selectedBarangay = ref('');
+    const addCustomer = async () => {
+        const customer = {
+            customerName: customerName.value,
+            address: `${selectedBarangay.value}, ${selectedMunicipality.value}`,
+            createdDate: new Date().toDateString(),
+            status: 'Active',
+        } as Customer;
+        try {
+            await addUserData(customer);
+            fetchCustomers();
+            window.alert('User data added successfully');
+        } catch (error) {
+            window.alert(`Error adding user data:', ${error}`);
+        }
+    };
 
-const addCustomer = async ()  => {
-    const customer = ref({
-        customerName: customerName.value,
-        address: `${selectedBarangay.value}, ${selectedMunicipality.value}`,
-        createdDate: new Date().toDateString(),
-        status: 'Active',
-    } as Customer);
-    try {
-        //console.log(customer.value);
-        await addUserData(customer.value);
-        fetchCustomers();
-        window.alert('User data added successfully');
-    } catch (error) {
-        window.alert(`Error adding user data:', ${error}`);
-    }
-};
-
-    return{
+    return {
         selectedBarangay,
         selectedMunicipality,
         customerName,
         addCustomer,
     };
-
 }
-
-
